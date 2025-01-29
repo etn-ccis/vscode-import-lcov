61089a1709781359d50a15ae6fad5a6cc0ee7149
johnchapman-eaton <johnchapman@eaton.com>
2025-01-29 13:04:48 -0500

    Added some logging and working to get this working.

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,10 @@ interface Configuration {
   lcovFiles: string[];
 }
 
+let coverageProfile: vscode.TestRunProfile | undefined;
+
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Extension "import-lcov" is now active!');
   // Test controller.
   // -----
   const controller = vscode.tests.createTestController(
@@ -18,7 +21,167 @@ export function activate(context: vscode.ExtensionContext) {
   let currentRefreshCts = new vscode.CancellationTokenSource();
   let configuration: Configuration = { lcovFiles: [] };
 
+  const createCoverageProfile = () => {
+    if (coverageProfile) {
+      coverageProfile.dispose();
+    }
+    console.log("Running coverage...");
+    coverageProfile = controller.createRunProfile(
+      "Coverage",
+      vscode.TestRunProfileKind.Coverage,
+      async (request, token) => {
+        const run = controller.createTestRun(
+          request,
+          /*name=*/ undefined,
+          /*persist=*/ true, // Set persist to true if you want to persist the results
+        );
+
+        // Compute items whose coverage should be processed.
+        const items = new Set(request.include);
+
+        if (items.size === 0) {
+          for (const [, item] of controller.items) {
+            items.add(item);
+          }
+        }
+
+        for (const item of request.exclude ?? []) {
+          items.delete(item);
+        }
+
+        // Read and update coverage.
+        await Promise.all([...items].map(async (item) => {
+          const uri = item.uri!;
+          const contents = await vscode.workspace.fs.readFile(uri);
+
+          if (token.isCancellationRequested) {
+            return;
+          }
+
+          const sections = await parseLcov({ from: contents });
+
+          if (token.isCancellationRequested) {
+            return;
+          }
+
+          for (const section of sections) {
+            let path: vscode.Uri | undefined;
+
+            for (
+              const workspaceFolder of vscode.workspace.workspaceFolders ?? []
+            ) {
+              const workspacePath = workspaceFolder.uri.fsPath;
+
+              if (section.path.startsWith(workspacePath)) {
+                path = vscode.Uri.joinPath(
+                  workspaceFolder.uri,
+                  section.path.substring(workspacePath.length + 1 /* / */),
+                );
+                break;
+              }
+            }
+
+            const fileCoverage = new vscode.FileCoverage(
+              path ?? vscode.Uri.file(section.path),
+              /*statementCoverage=*/ new vscode.TestCoverageCount(
+                section.lines.hit,
+                section.lines.instrumented,
+              ),
+              /*branchCoverage=*/ new vscode.TestCoverageCount(
+                section.branches.hit,
+                section.branches.instrumented,
+              ),
+              /*declarationCoverage=*/ new vscode.TestCoverageCount(
+                section.functions.hit,
+                section.functions.instrumented,
+              ),
+            );
+
+            testRunData.set(fileCoverage, section);
+            run.addCoverage(fileCoverage);
+          }
+        }));
+
+        run.end();
+      },
+    );
+    
+    const testRunData = new WeakMap<vscode.FileCoverage, SectionSummary>();
+  
+    let demangle:
+      | undefined
+      | Promise<{ (mangled: string): string }>
+      | { (mangled: string): string };
+  
+    coverageProfile.loadDetailedCoverage = async (_, fileCoverage) => {
+      const section = testRunData.get(fileCoverage);
+  
+      if (section === undefined) {
+        return [];
+      }
+  
+      const details: vscode.FileCoverageDetail[] = [];
+      const branchesByLine = new Map<
+        number,
+        Record<string, vscode.BranchCoverage>
+      >();
+  
+      for (const branch of section.branches.details) {
+        const branches = branchesByLine.get(branch.line) ?? {};
+  
+        branches[branch.branch] ??= new vscode.BranchCoverage(
+          0,
+          new vscode.Position(branch.line - 1, 0),
+          branch.branch,
+        );
+        (branches[branch.branch].executed as number) += branch.hit;
+  
+        branchesByLine.set(branch.line, branches);
+      }
+  
+      for (const line of section.lines.details) {
+        details.push(
+          new vscode.StatementCoverage(
+            line.hit,
+            new vscode.Position(line.line - 1, 0),
+            Object.values(branchesByLine.get(line.line) ?? {}),
+          ),
+        );
+      }
+  
+      for (const fn of section.functions.details) {
+        if (fn.name.length === 0) {
+          continue;
+        }
+  
+        let name = fn.name;
+  
+        if (/^_{1,3}Z/.test(name)) {
+          if (demangle === undefined) {
+            demangle = loadDemangle(context);
+            demangle = await demangle;
+          } else if (demangle instanceof Promise) {
+            demangle = await demangle;
+          }
+  
+          name = demangle(name);
+        }
+  
+        details.push(
+          new vscode.DeclarationCoverage(
+            name,
+            fn.hit,
+            new vscode.Position(fn.line - 1, 0),
+          ),
+        );
+      }
+  
+      return details;
+    };    
+  };
+
   const refreshTests = async () => {
+    console.log("Refreshing tests...");
     currentRefreshCts.cancel();
     currentRefreshCts = new vscode.CancellationTokenSource();
 
@@ -44,6 +207,7 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     controller.items.replace(items);
+    createCoverageProfile();
   };
 
   controller.refreshHandler = refreshTests;
@@ -53,6 +217,7 @@ export function activate(context: vscode.ExtensionContext) {
   const fileSystemWatchers = new Map<string, vscode.FileSystemWatcher>();
 
   const refreshConfiguration = () => {
+    console.log('Refreshing configuration...');
     let lcovFiles = vscode.workspace.getConfiguration("import-lcov").get<
       string | string[]
     >("lcovFiles") ?? [];
@@ -73,6 +238,7 @@ export function activate(context: vscode.ExtensionContext) {
     const watchersToRemove = new Set(fileSystemWatchers.keys());
 
     for (const lcovFile of lcovFiles) {
+      console.log(`Watching: ${lcovFile}`);
       if (watchersToRemove.delete(lcovFile)) {
         // Already watching this glob.
         continue;
@@ -81,11 +247,15 @@ export function activate(context: vscode.ExtensionContext) {
       const watcher = vscode.workspace.createFileSystemWatcher(
         lcovFile,
         /*ignoreCreateEvents*/ false,
-        /*ignoreChangeEvents*/ true,
+        /*ignoreChangeEvents*/ false,
         /*ignoreDeleteEvents*/ false,
       );
 
       watcher.onDidCreate(refreshTests);
+      watcher.onDidChange(() => {
+        console.log(`File changed: ${lcovFile}`);
+        refreshTests();
+      });
       watcher.onDidDelete((uri) => controller.items.delete(uri.toString()));
 
       fileSystemWatchers.set(lcovFile, watcher);
@@ -117,163 +287,12 @@ export function activate(context: vscode.ExtensionContext) {
       },
     },
   );
-
-  // Test runs.
-  // -----
-
-  const testRunData = new WeakMap<vscode.FileCoverage, SectionSummary>();
-  const coverageProfile = controller.createRunProfile(
-    "Coverage",
-    vscode.TestRunProfileKind.Coverage,
-    async (request, token) => {
-      const run = controller.createTestRun(
-        request,
-        /*name=*/ undefined,
-        /*persist=*/ false,
-      );
-
-      // Compute items whose coverage should be processed.
-      const items = new Set(request.include);
-
-      if (items.size === 0) {
-        for (const [, item] of controller.items) {
-          items.add(item);
-        }
-      }
-
-      for (const item of request.exclude ?? []) {
-        items.delete(item);
-      }
-
-      // Read and update coverage.
-      await Promise.all([...items].map(async (item) => {
-        const uri = item.uri!;
-        const contents = await vscode.workspace.fs.readFile(uri);
-
-        if (token.isCancellationRequested) {
-          return;
-        }
-
-        const sections = await parseLcov({ from: contents });
-
-        if (token.isCancellationRequested) {
-          return;
-        }
-
-        for (const section of sections) {
-          let path: vscode.Uri | undefined;
-
-          for (
-            const workspaceFolder of vscode.workspace.workspaceFolders ?? []
-          ) {
-            const workspacePath = workspaceFolder.uri.fsPath;
-
-            if (section.path.startsWith(workspacePath)) {
-              path = vscode.Uri.joinPath(
-                workspaceFolder.uri,
-                section.path.substring(workspacePath.length + 1 /* / */),
-              );
-              break;
-            }
-          }
-
-          const fileCoverage = new vscode.FileCoverage(
-            path ?? vscode.Uri.file(section.path),
-            /*statementCoverage=*/ new vscode.TestCoverageCount(
-              section.lines.hit,
-              section.lines.instrumented,
-            ),
-            /*branchCoverage=*/ new vscode.TestCoverageCount(
-              section.branches.hit,
-              section.branches.instrumented,
-            ),
-            /*declarationCoverage=*/ new vscode.TestCoverageCount(
-              section.functions.hit,
-              section.functions.instrumented,
-            ),
-          );
-
-          testRunData.set(fileCoverage, section);
-          run.addCoverage(fileCoverage);
-        }
-      }));
-
-      run.end();
-    },
-  );
-
-  let demangle:
-    | undefined
-    | Promise<{ (mangled: string): string }>
-    | { (mangled: string): string };
-
-  coverageProfile.loadDetailedCoverage = async (_, fileCoverage) => {
-    const section = testRunData.get(fileCoverage);
-
-    if (section === undefined) {
-      return [];
-    }
-
-    const details: vscode.FileCoverageDetail[] = [];
-    const branchesByLine = new Map<
-      number,
-      Record<string, vscode.BranchCoverage>
-    >();
-
-    for (const branch of section.branches.details) {
-      const branches = branchesByLine.get(branch.line) ?? {};
-
-      branches[branch.branch] ??= new vscode.BranchCoverage(
-        0,
-        new vscode.Position(branch.line - 1, 0),
-        branch.branch,
-      );
-      (branches[branch.branch].executed as number) += branch.hit;
-
-      branchesByLine.set(branch.line, branches);
-    }
-
-    for (const line of section.lines.details) {
-      details.push(
-        new vscode.StatementCoverage(
-          line.hit,
-          new vscode.Position(line.line - 1, 0),
-          Object.values(branchesByLine.get(line.line) ?? {}),
-        ),
-      );
-    }
-
-    for (const fn of section.functions.details) {
-      if (fn.name.length === 0) {
-        continue;
-      }
-
-      let name = fn.name;
-
-      if (/^_{1,3}Z/.test(name)) {
-        if (demangle === undefined) {
-          demangle = loadDemangle(context);
-          demangle = await demangle;
-        } else if (demangle instanceof Promise) {
-          demangle = await demangle;
-        }
-
-        name = demangle(name);
-      }
-
-      details.push(
-        new vscode.DeclarationCoverage(
-          name,
-          fn.hit,
-          new vscode.Position(fn.line - 1, 0),
-        ),
-      );
-    }
-
-    return details;
-  };
 }
 
 export function deactivate() {
-  // Nop.
+  console.log('Extension "import-lcov" is now deactivated');
+  // Clean up resources, if necessary
+  if (coverageProfile) {
+    coverageProfile.dispose();
+  }
 }
